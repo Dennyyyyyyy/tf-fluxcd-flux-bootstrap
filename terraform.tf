fc6856cf9865c733f382d8e364fbb6ef18beb524
Denys <127623376+Dennyyyyyyy@users.noreply.github.com>
2024-01-07 00:20:04 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     flux = {
       source  = "fluxcd/flux"
-      #version = "1.0.0-rc.3"
-      version = "1.2.1"
+      version = "1.0.0-rc.3"
+      #version = "1.2.1"
     }
   }
 }
